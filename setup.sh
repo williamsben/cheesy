6942f04e111b81d309d355abcfd189e70c1c82cb
Ben Williams <ben.williams@e-gineering.com>
2017-03-10 20:32:47 -0500

    checking for homebrew

--- setup.sh
+++ setup.sh
@@ -3,5 +3,20 @@
 echo "Checking for Homebrew..."
 command -v brew >/dev/null && echo "Homebrew installed." || {
     echo "Homebrew not installed.";
-    echo "Installing Homebrew";
+    echo "Installing Homebrew...";
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 }
+
+brew update
+
+brew install caskroom/cask/brew-cask
+
+brew cask install docker-toolbox
+
+docker-machine create --driver "virtualbox" default
+
+docker-machine start default
+
+eval "$(docker-machine env default)"
+
+docker run -it ubuntu
